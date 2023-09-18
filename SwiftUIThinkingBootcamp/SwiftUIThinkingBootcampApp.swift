9c7fb426f5fceffa297c1e9aa37e4ccb166c4307
polosa00 <i@apolochanin.ru>
2023-09-18 20:46:06 +0300

    Initial Commit

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampApp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Александр Полочанин on 18.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
